1f05f513bc6fcd4dc709890ae42f4355ff7cb703
HARU Akebono <haru_ake@outlook.jp>
2016-10-14 01:57:39 +0900

    add hash ckeck

--- Casks/kcd.rb
+++ Casks/kcd.rb
@@ -1,6 +1,6 @@
 cask 'kcd' do
   version '1.7-beta14'
-  sha256 :no_check
+  sha256 'a6939d3e884fb3e7fb385caa27ea795a79834d2eea8c4f1e4dd39d659301f1e9'
 
   url "http://jaist.dl.osdn.jp/kcd/66568/KCD-1.7b14-c782333.zip"
   name '艦娘は今日も元気です。'
